1ae8527f66711e8dab458b75c594fe64bd91929d
saurabh.nahar <saurabh.n.nahar@gmail.com>
2023-08-22 08:20:46 -0400

    compact code

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 let body = document.querySelector('body')
 
-let btn1 = document.querySelector('#box1')
-let btn2 = document.querySelector('#box2')
-let btn3 = document.querySelector('#box3')
-let btn4 = document.querySelector('#box4')
+// let btn1 = document.querySelector('#box1')
+// let btn2 = document.querySelector('#box2')
+// let btn3 = document.querySelector('#box3')
+// let btn4 = document.querySelector('#box4')
 
-btn1.addEventListener("click", () => {
-    body.style.backgroundColor = "red"
-}
-);
+// btn1.addEventListener("click", () => {
+//     body.style.backgroundColor = "red"
+// }
+// );
 
-btn2.addEventListener("click", () => {
-    body.style.backgroundColor = "green"
-}
-);
+// btn2.addEventListener("click", () => {
+//     body.style.backgroundColor = "green"
+// }
+// );
 
-btn3.addEventListener("click", () => {
-    body.style.backgroundColor = "blue"
-}
-);
+// btn3.addEventListener("click", () => {
+//     body.style.backgroundColor = "blue"
+// }
+// );
 
-btn4.addEventListener("click", () => {
-    body.style.backgroundColor = "yellow"
-}
-);
+// btn4.addEventListener("click", () => {
+//     body.style.backgroundColor = "yellow"
+// }
+// );
+
+let btn = document.querySelectorAll('button');
+
+btn.forEach(function (button) {
+    button.addEventListener("click", (e) => {
+        if(e.target.id === "red"){
+            body.style.backgroundColor = "red";
+        }if(e.target.id === "green"){
+            body.style.backgroundColor = "green";
+        }if(e.target.id === "blue"){
+            body.style.backgroundColor = "blue";
+        }if(e.target.id === "yellow"){
+            body.style.backgroundColor = "yellow";
+        }
+    })
+});
+
